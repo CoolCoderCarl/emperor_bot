57400fd1a23b05fa718d1c145635ed73f0327cc6
Ernest <eumerov@fix.ru>
2020-05-22 15:15:20 +0300

    Update funcs

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ bot = telebot.TeleBot(token='1263231073:AAGvUob4MTimlFzAKfdLDTjySnpxEhnHqxg')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message, "Howdy, how are you doing?")
-
-# @bot.message_handler(func=lambda message: True)
-# def echo_all(message):
-#   bot.reply_to(message, message.text)
+  bot.reply_to(message, "STOP CLICKING THAT STUPID BUTTONS, PEASANT !")
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -16,7 +12,9 @@ def send_text(message):
       bot.send_message(message.chat.id, 'ПРИВЕТСТВУЮ ТЕБЯ, КРЕСТЬЯНИН.')
     elif message.text == 'Пока':
       bot.send_message(message.chat.id, 'ЗАВОД ЖДЁТ !')
-    elif message.text == 'FOR THE EMPEROR !' or 'ЗА ИМПЕРАТОРА !':
-      bot.send_message(message.chat.id, 'YEAH !')
+    elif message.text == 'ЗА ИМПЕРАТОРА !':
+      bot.send_message(message.chat.id, 'ХОРОШИЙ КРЕСТЬЯНИН !')
+    else:
+        bot.send_message(message.chat.id, 'ГОВОРИ НОРМАЛЬНО !')
 
 bot.polling()
