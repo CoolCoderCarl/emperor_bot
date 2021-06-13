549007399e565093030586ab8edaa893d924e93d
CoolCoderCarl <123qwekir2wl@gmail.com>
2021-06-13 14:21:30 +0300

    Add items; change content and add jokes

---
+++ items/eternal_wisdom.py
@@ -0,0 +1,47 @@
+eternal_wisdom = [
+
+    '''
+        The orc turned the mug upside down and came up to BigMek: - Mek at my mug has a neck sealed !!! 
+        Mek looks at the mug: - Eh rookie, you are really stupid, it also has no bottom!
+    ''',
+
+    '''
+        The inquisitor asks the infantryman:
+        - Well, how is the Librarian doing after being wounded?
+        - Already got on skis!
+        - Wow! And I only get up on women!
+    ''',
+
+    'Yesterday on Manezhnaya Square there was a fight between Warhammer fans and Warcraft fans ... OMON fans won !!!',
+
+    '''
+        Commissar to the Guards:
+        - And now you have a march. But the bosses cannot run. In peacetime, this causes laughter, and in wartime, panic.
+    ''',
+
+    '''
+        Private GWard won the short story competition (300 words).
+        Here are its story:
+        "Our sergeant ordered to build a separate toilet for himself, where he would not let anyone. 
+        We got angry and sawed down the posts on which this toilet stood. There are 21 words. 
+        The remaining 279 words were said by our sergeant, getting out of the cesspool."
+    ''',
+    '''
+        - Mr. chaoslord, why is there such black smoke coming from that chimney?
+        - Probably, they forgot to take off your galoshes from your dad ...
+    ''',
+
+    '''
+        Conversation between cultists:
+        - Arkams I do not love my little brother!
+        - Keep quiet and eat what they give!
+    ''',
+
+    '''
+        A passenger ship is flying on one of the planets of the Imperium: the stewardess asks.
+        - Is there a doctor in the compartment?
+        One gets up and goes to the cockpit. In 2 minutes.
+        - Is there a pilot in the compartment?
+    '''
+
+]
