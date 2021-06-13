d98e7dd9fac409eba505fd5ee4ec4796eb805c6f
CoolCoderCarl <123qwekir2wl@gmail.com>
2021-06-13 14:43:29 +0300

    Fixed dependencies; update messages

--- items/eternal_wisdom.py
+++ items/eternal_wisdom.py
@@ -1,73 +1,74 @@
 eternal_wisdom = [
 
     '''
-        The orc turned the mug upside down and came up to BigMek: - Mek at my mug has a neck sealed !!! 
-        Mek looks at the mug: - Eh rookie, you are really stupid, it also has no bottom!
+The orc turned the mug upside down and came up to BigMek: - Mek at my mug has a neck sealed !!! \n
+Mek looks at the mug: - Eh rookie, you are really stupid, it also has no bottom!
     ''',
 
     '''
-        The inquisitor asks the infantryman:
-        - Well, how is the Librarian doing after being wounded?
-        - Already got on skis!
-        - Wow! And I only get up on women!
+The inquisitor asks the infantryman:\n
+- Well, how is the Librarian doing after being wounded?\n
+- Already got on skis!\n
+- Wow! And I only get up on women!
     ''',
 
     'Yesterday on Manezhnaya Square there was a fight between Warhammer fans and Warcraft fans ... OMON fans won !!!',
 
     '''
-        Commissar to the Guards:
-        - And now you have a march. But the bosses cannot run. In peacetime, this causes laughter, and in wartime, panic.
+Commissar to the Guards:\n
+- And now you have a march. But the bosses cannot run. In peacetime, this causes laughter, and in wartime, panic.
     ''',
 
     '''
-        Private GWard won the short story competition (300 words).
-        Here are its story:
-        "Our sergeant ordered to build a separate toilet for himself, where he would not let anyone. 
-        We got angry and sawed down the posts on which this toilet stood. There are 21 words. 
-        The remaining 279 words were said by our sergeant, getting out of the cesspool."
-    ''',
-    '''
-        - Mr. chaoslord, why is there such black smoke coming from that chimney?
-        - Probably, they forgot to take off your galoshes from your dad ...
+Private GWard won the short story competition (300 words).\n
+Here are its story:\n
+"Our sergeant ordered to build a separate toilet for himself, where he would not let anyone. 
+We got angry and sawed down the posts on which this toilet stood. There are 21 words.
+The remaining 279 words were said by our sergeant, getting out of the cesspool."
     ''',
 
     '''
-        Conversation between cultists:
-        - Arkams I do not love my little brother!
-        - Keep quiet and eat what they give!
+- Mr. chaoslord, why is there such black smoke coming from that chimney?\n
+- Probably, they forgot to take off your galoshes from your dad ...
     ''',
 
     '''
-        A passenger ship is flying on one of the planets of the Imperium: the stewardess asks.
-        - Is there a doctor in the compartment?
-        One gets up and goes to the cockpit. In 2 minutes.
-        - Is there a pilot in the compartment?
+Conversation between cultists:\n
+- Arkams I do not love my little brother!\n
+- Keep quiet and eat what they give!
     ''',
 
     '''
-        The doorbell rings ... in the middle of the night ...
-        - Who's there?
-        - Inquisition!
-        - And no one is at home!
-        - And who speaks then?
-        - This is the radio!
-        - Why is the light on?
-        - I'll turn it off right now!
+A passenger ship is flying on one of the planets of the Imperium: the stewardess asks.\n
+- Is there a doctor in the compartment?\n
+One gets up and goes to the cockpit. In 2 minutes.\n
+- Is there a pilot in the compartment?
     ''',
 
     '''
-        Commissar:
-        - Tomorrow we arrive at seven o'clock.
-        Question from the line:
-        - At seven in the morning or at seven in the evening?
-        Commissar:
-        - At seven in the afternoon.
+The doorbell rings ... in the middle of the night ...\n
+- Who's there?\n
+- Inquisition!\n
+- And no one is at home!\n
+- And who speaks then?\n
+- This is the radio!\n
+- Why is the light on?\n
+- I'll turn it off right now!
     ''',
 
     '''
-        - Sir! Sir! We're out of cartridges!
-        - Has your faith in the Emperor dried up ?!
-        - No !!! - answered the spacemarines and continued to shoot.
+Commissar:\n
+- Tomorrow we arrive at seven o'clock.\n
+Question from the line:\n
+- At seven in the morning or at seven in the evening?\n
+Commissar:\n
+- At seven in the afternoon.
+    ''',
+
+    '''
+- Sir! Sir! We're out of cartridges!\n
+- Has your faith in the Emperor dried up ?!\n
+- No !!! - answered the spacemarines and continued to shoot.
     '''
 
 ]
